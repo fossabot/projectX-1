aa87278f6f9bca5d28865b2e117db6d5a3e01dab
Rizwan Rana <RizwanR@rmt.otl>
2017-02-09 00:24:48 -0500

    Added reverse order stuff

--- helloWorld.go
+++ helloWorld.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println("Hello World!")
+	var input int
+	fmt.Println("kuch to likh bc:")
+	fmt.Scanf("%v", &input)
+
+	s := make([]int, input)
+
+	for i := 0; i < input; i++ {
+		fmt.Scanf("%v", &s[i])
+	}
+
+	/*for _, value := range s {
+	//	fmt.Println(value)
+	//	sum += value
+	*/
+
+	for j := input - 1; j >= 0; j-- {
+		fmt.Println(s[j])
+	}
 }
